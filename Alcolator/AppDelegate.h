a79ec3e3b923516fac67859de58548183db0299b
Matti Salokangas <matti.salokangas@gmail.com>
2015-10-23 19:59:05 -0700

    Initial Project Files

---
+++ Alcolator/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Alcolator
+//
+//  Created by Matti Salokangas on 10/23/15.
+//  Copyright © 2015 Sturdy Nut. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Alcolator/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Alcolator
+//
+//  Created by Matti Salokangas on 10/23/15.
+//  Copyright © 2015 Sturdy Nut. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
